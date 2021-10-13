b53bd7562fe90648f162e29a0c3efd9971b612cd
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-10-13 05:31:42 +0100

    add backgoround color

--- src/commons/boundary/styles.ts
+++ src/commons/boundary/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const LoaderWrapper = styled.div`
+  background-color: rgba(36, 13, 133, 0.3);
   height: 100vh;
   display: flex;
   justify-content: center;

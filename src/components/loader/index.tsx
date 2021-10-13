d98f8c412e93518954c1a00cdca85cea2fd49d61
chidiogo-akunne <chidiogoakunne@gmail.com>
2021-10-13 04:04:28 +0100

    design spinner

---
+++ src/components/loader/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { SpringSpinner } from "react-epic-spinners";
+
+interface loaderProp {
+  spinnerSize?: number;
+}
+
+export default function Loader(props: loaderProp) {
+  const { spinnerSize } = props;
+  let size = 40;
+  let windowWidth = window.innerWidth;
+  windowWidth > 700 ? (size = 70) : (size = 40);
+
+  return <SpringSpinner color="rgb(102 77 210)" size={spinnerSize || size} />;
+}
